20c33d83f3a461dc2c4cc3834e97f03e3d170b6c
Jon Yoder <jsyoder@mailfence.com>
2020-03-06 19:46:33 -0500

    Add message and add inheritance

--- items.py
+++ items.py
@@ -1,12 +1,35 @@
-class Note:
+class ClientItem(self):
 	def __init__(self):
 		self.id = ''
+		self.type = ''
+		self.attachments = list()
+
+
+class Message(ClientItem):
+	def __init__(self):
+		ClientItem.__init__(self)
+		self.type = 'message'
+		self.version = 1.0
+		self.sender = ''
+		self.recipients = list()
+		self.ccrecipients = list()
+		self.bccrecipients = list()
+		self.date = ''
+		self.subject = ''
+		self.body = ''
+		self.thread_id = ''
+
+
+class Note(ClientItem):
+	def __init__(self):
+		ClientItem.__init__(self)
+		self.type = 'note'
+		self.version = 1.0
 		self.title = ''
 		self.body = ''
 		self.notebook = ''
 		self.created = ''
 		self.updated = ''
 		self.tags = list()
-		self.attachments = list()
 
 
