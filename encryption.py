68a9833dbe03f61f6486c69091ebf55fecff9c5c
Jon Yoder <jsyoder@mailfence.com>
2020-04-04 11:49:45 -0400

    Implement FolderMapping class

--- encryption.py
+++ encryption.py
@@ -99,3 +99,23 @@ class SecretKey (EncryptionKey):
 		'''Returns the key encoded in base64'''
 		return self.key64
 	
+
+class FolderMapping:
+	'''Represents the mapping of a server-side path to a local one'''
+	def __init__(self):
+		self.fid = ''
+		self.address = ''
+		self.keyid = ''
+		self.path = ''
+		self.permissions = ''
+	
+	def MakeID(self):
+		'''Generates a FID for the object'''
+		self.fid = str(uuid.uuid4())
+
+	def Set(self, address, keyid, path, permissions):
+		'''Sets the values of the object'''
+		self.address = address
+		self.keyid = keyid
+		self.path = path
+		self.permissions = permissions
