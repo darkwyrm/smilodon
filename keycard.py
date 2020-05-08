ad801522bab5e12e77535d925666ed47d3da5fbd
Jon Yoder <jsyoder@mailfence.com>
2020-05-08 19:32:36 -0400

    Finish implementing UserCard

--- keycard.py
+++ keycard.py
@@ -23,6 +23,8 @@ def generate_encryption_key():
 	keypair['public'] = key.public_key.encode()
 	return keypair
 
+class ComplianceException(Exception):
+	pass
 
 class __CardBase:
 	'''Represents an organizational keycard'''
@@ -228,6 +230,108 @@ class UserCard(__CardBase):
 		]
 		self.fields['Time-To-Live'] = '7'
 		self.set_expiration()
+
+	def is_compliant(self):
+		'''Checks the fields to ensure that it meets spec requirements. If a field causes it 
+		to be noncompliant, the noncompliant field is also returned'''
+		status, bad_field = super().is_compliant()
+		if not status:
+			return status, bad_field
+		
+		if 'User' not in self.signatures or not self.signatures['User']:
+			return False, 'User-Signature'
+		
+		if 'Organization' not in self.signatures or not self.signatures['Organization']:
+			return False, 'Organizational-Signature'
+		
+		return True, ''
+
+	def set_from_string(self, text):
+		'''Initializes the keycard from string data'''
+		super().set_from_string(text)
+		
+		signatures = {
+			'User-Signature':'User',
+			'Custody-Signature':'Custody',
+			'Organization-Signature':'Organization'
+		}
+		for k,v in signatures.items():
+			if k in self.fields:
+				self.signatures[v] = self.fields[k]
+				del self.fields[k]
+		
+	def __str__(self):
+		lines = list()
+		if self.type:
+			lines.append("Type:" + self.type)
+		
+		for field in self.field_names:
+			if field in self.fields and self.fields[field]:
+				lines.append("%s:%s" % (field, self.fields[field]))
+		
+		for sig in [ 'Custody', 'User', 'Organization' ]:
+			if sig in self.signatures and self.signatures[sig]:
+				lines.append(''.join([sig, '-Signature:', self.signatures['User']]))
+		
+		return '\n'.join(lines)
+
+	def sign(self, signing_key, sigtype):
+		'''Adds a signature to the keycard. Note that for any change in the keycard fields, this 
+		call must be made afterward. Note that successive signatures are deleted, such that 
+		updating a User signature will delete the Organization signature which depends on it. The 
+		sigtype must be Custody, User, or Organization, and the type is case-sensitive.'''
+		if not signing_key or sigtype not in ['Custody', 'User', 'Organization']:
+			raise ValueError
+		
+		if not isinstance(signing_key, bytes):
+			raise TypeError
+		
+		key = nacl.signing.SigningKey(signing_key)
+		parts = [ super().__str__() ]
+		
+		if sigtype == 'Custody':
+			if 'User' in self.signatures:
+				del self.signatures['User']
+				del self.signatures['Organization']
+		
+		if sigtype == 'User':
+			if 'User' in self.signatures:
+				del self.signatures['Organization']
+			if self.signatures['Custody']:
+				parts.append('Custody-Signature:' + self.signatures['Custody'] + '\n')
+		elif sigtype == 'Orgnization':
+			if not self.signatures['User']:
+				raise ComplianceException
+			parts.append('User-Signature:' + self.signatures['User'] + '\n')
+		
+		signed = key.sign(''.join(parts).encode(), Base85Encoder)
+		self.signatures[sigtype] = signed.signature.decode()
+
+	def verify(self, verify_key, sigtype):
+		'''Verifies a signature, given a verification key'''
+		if sigtype not in self.signatures or not self.signatures[sigtype]:
+			raise ValueError
+		
+		vkey = nacl.signing.VerifyKey(Base85Encoder.decode(verify_key))
+		parts = [ super().__str__() ]
+		
+		if sigtype == 'User' and 'Custody' in self.signatures:
+			if self.signatures['Custody']:
+				parts.append('Custody-Signature:' + self.signatures['Custody'] + '\n')
+			else:
+				# The Custody-Signature field must be populated if it exists
+				raise ComplianceException
+		elif sigtype == 'Orgnization':
+			if 'User' not in self.signatures or not self.signatures['User']:
+				raise ComplianceException
+			parts.append('User-Signature:' + self.signatures['User'] + '\n')
+		
+		try:
+			vkey.verify(''.join(parts).encode(), Base85Encoder.decode(self.signatures[sigtype]))
+		except nacl.exceptions.BadSignatureError:
+			return False
+		
+		return True
 
 
 class Base85Encoder:
