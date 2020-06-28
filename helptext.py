d4c61d7779c4228b7685ccdc1fd8a5a55a94480d
Jon Yoder <jsyoder@mailfence.com>
2020-06-28 09:18:17 -0400

    Update LOGIN and add SETINFO

--- helptext.py
+++ helptext.py
@@ -4,10 +4,15 @@ ensure the code remains easy to read.'''
 login_cmd = '''Usage: login <address>
 Log into a server once connected. The address used may be the numeric address
 (e.g. 557207fd-0a0a-45bb-a402-c38461251f8f) or the friendly address (e.g. 
-CatLover). It is not necessary to give the entire workspace address
-(CatLover/example.com), but it will not cause any errors if used. If a
-friendly address contains spaces, it must be enclosed in double quotes, as in
-"John Q. Public/example.com" or "John Q. Public".'''
+CatLover). Because multiple domains could be hosted by the same server, it is 
+necessary to give the entire workspace address. Note that if the address is 
+omitted, the command is assumed to log into main account for the profile.
+
+Examples:
+login f009338a-ea14-4d59-aa48-016829835cd7/example.com
+login CatLover/example.com
+login
+'''
 
 profile_cmd = '''Usage: profile <action> <profilename>
 Manage profiles. Actions are detailed below.
@@ -43,3 +48,9 @@ Executes a command directly in the regular user shell. On Windows, this is
 Command Prompt. On UNIX-like platforms, this is the default shell, usually
 bash.
 Aliases: ` , sh'''
+
+setinfo_cmd = '''Usage: setinfo <infotype> <value>
+Sets contact information for the profile. Available information which can be set is listed below:
+
+address - the "friendly name" for the workspace, i.e. the non-numeric part of a person's Anselus address which is what is usually shared with other people.
+'''
