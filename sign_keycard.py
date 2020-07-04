2bab448c7b2765558870b23ebd50383019067700
Jon Yoder <jsyoder@mailfence.com>
2020-07-03 20:32:25 -0400

    Add some more basic testing

--- sign_keycard.py
+++ sign_keycard.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-# import encryption
+import encryption
 import keycard
 
 def print_usage():
@@ -16,10 +16,31 @@ debug_app = True
 
 if __name__ == '__main__':
 	if debug_app:
-		status = keycard.load_keycard('testcards/org.keycard')
+		status = keycard.load_keycard('testcards/user.keycard')
 		if status.error():
-			print("Load failure: %s" % status.info())
+			print("User keycard load failure: %s" % status.info())
+			sys.exit(0)
 		else:
-			print(status['card'])
-		sys.exit(0)
+			print("User keycard:\n%s\n" % str(status['card']))
+		user_card = status['card']
+		
+		status = encryption.load_signingpair('testcards/user_signing_keypair.jk')
+		if status.error():
+			print("Signing key load failure: %s" % status.info())
+			sys.exit(0)
+		else:
+			print("User signing key:\n%s\n" % str(status['keypair']))
+		user_key = status['keypair']
 
+		status = encryption.load_signingpair('testcards/org_signing_keypair.jk')
+		if status.error():
+			print("Signing key load failure: %s" % status.info())
+			sys.exit(0)
+		else:
+			print("Organization signing key:\n%s\n" % str(status['keypair']))
+		org_key = status['keypair']
+
+		user_card.sign(user_key.private, 'User')
+		print("After user signature:\n%s\n" % str(user_card))
+		user_card.sign(org_key.private, 'Organization')
+		print("After organization signature:\n%s\n" % str(user_card))
